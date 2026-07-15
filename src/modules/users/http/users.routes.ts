82cf7271c83a8a82b77ef54d9d3cf6003931cd01
Jordy Silva <jordy.silva@associate.us.idemia.com>
2026-07-15 19:08:22 -0300

    add users.route -- to be completed

---
+++ src/modules/users/http/users.routes.ts
@@ -0,0 +1,31 @@
+export const usersRoutes = async (app: any) => {
+  app.post(
+    '/register',
+    {
+      schema: {
+        body: {
+          type: 'object',
+          properties: {
+            email: { type: 'string', format: 'email' },
+            password: { type: 'string', minLength: 8 },
+          },
+          required: ['email', 'password'],
+        },
+        response: {
+          200: { type: 'object', properties: { token: { type: 'string' } } },
+        },
+      },
+    },
+    async (request: any, reply: any) => {
+      const { email, password } = request.body;
+
+      const user = await reply.registerUserService.execute({ email, password });
+
+      return reply
+        .status(200)
+        .send({
+          token: reply.jwtSign({ role: 'user' }, { sign: { sub: user.id, expiresIn: '1d' } }),
+        });
+    },
+  );
+};
